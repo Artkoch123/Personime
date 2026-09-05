651c5b56b7ad50c0deb7fa7324635c1754f5f3e9
Artkoch123 <sevegg12@gmail.com>
2026-09-05 12:39:29 +0900

    feat: add quiz mode selection page

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ app.config['secret_key'] = ''
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html", new="lol")
+    return render_template("index.html", title="main")
+
+@app.route('/quiz')
+def quiz():
+    return render_template("quiz.html", title="quiz")
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
