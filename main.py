8cf8a1bb0bff11375053441ed586f500917b0185
Artkoch123 <sevegg12@gmail.com>
2026-09-05 11:15:00 +0900

    add landing page with hero and how-it-works cards

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from flask import Flask, render_template
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+app.config['secret_key'] = ''
 
+@app.route('/')
+def hello_world():
+    return render_template("index.html", new="lol")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm') #fortnight balls
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(port=8080, host='127.0.0.1', debug=True)
